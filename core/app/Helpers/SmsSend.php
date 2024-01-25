5529ce458f9b6a2071c789910ba0d392299a3eac
Nes-cmd <Nesusadik0@gmail.com>
2024-01-25 23:30:36 +0300

    test

--- core/app/Helpers/SmsSend.php
+++ core/app/Helpers/SmsSend.php
@@ -8,14 +8,14 @@ class SmsSend {
     public static function send($to, $message){
         // if()
         $response =  Http::withHeaders([
-            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZGVudGlmaWVyIjoidkV0MjVTVWg2OG9MNWNBdFE1dWxHNlpicHJ3RzMxd1QiLCJleHAiOjE4NjEyMDA0OTEsImlhdCI6MTcwMzM0NzY5MSwianRpIjoiNzI1OWQzNzgtZGYwZS00NzIwLWJiOTctY2YxMzM4Njc3YTAwIn0.w58houRyXLAt8xaO6QbgUwnG3nHdVMTiKUHJQcH9AAk',
+            'Authorization' => 'Bearer '. env('AFRO_KEY'),
             'Content-type'  => 'application/json',
         ])->post('https://api.afromessage.com/api/send', [
-            'from'     => 'e80ad9d8-adf3-463f-80f4-7c4b39f7f164', //
-            'sender'   => '9786', // sender short code 
+            'from'     =>  env('AFRO_ID'), //
+            'sender'   => env('AFRO_SENDER'), // sender short code 
             'to'       => $to, 
             'message'  => $message,
-            'callback' => 'http://example.com'
+            'callback' => url('/')
         ]);
 
         return $response->json();
