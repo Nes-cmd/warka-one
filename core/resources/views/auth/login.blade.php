df77092715fb19befe2c83c7ff2b2545717ae47b
Nesru <nesrusadik6@gmail.com>
2024-02-28 22:53:42 +0300

    layout setup

--- core/resources/views/auth/login.blade.php
+++ core/resources/views/auth/login.blade.php
@@ -29,30 +29,34 @@
 
             <div class="h-16" x-show="authwith == 'phone'">
                 <x-input-label for="phone" :value="__('Phone')" />
-                <div class="relative mb-8">
-                    <x-text-input id="phone" class="block mt-1 md:w-[79%] w-[65%] absolute right-0  py-2" type="tel" name="phone" value="{{ old('phone')}}" />
-
-                    <div class="absolute left-0 py-1 md:w-[30%] w-[20%]">
+                
+                <div class="mb-8 flex">
+                    
+                    <div class="py-1">
                         <div x-data="{
-                        open: false,
-                        toggle() {
-                            if (this.open) {
-                                return this.close()
+                            open: false,
+                            toggle() {
+                                if (this.open) {
+                                    return this.close()
+                                }
+                                this.$refs.button.focus()
+                                this.open = true
+                            },
+                            close(focusAfter) {
+                                if (! this.open) return
+                                this.open = false
+                                focusAfter && focusAfter.focus()
                             }
-                            this.$refs.button.focus()
-                            this.open = true
-                        },
-                        close(focusAfter) {
-                            if (! this.open) return
-                            this.open = false
-                            focusAfter && focusAfter.focus()
-                        }
-                        }" x-on:keydown.escape.prevent.stop="close($refs.button)" x-on:focusin.window="! $refs.panel.contains($event.target) && close()" x-id="['dropdown-button']" class="relative">
+                            }" 
+                            x-on:keydown.escape.prevent.stop="close($refs.button)" 
+                            x-on:focusin.window="!$refs.panel.contains($event.target) && close()" 
+                            x-id="['dropdown-button']" 
+                            class="relative"
+                            >
                             <!-- Button -->
-                            <button type="button" x-ref="button" x-on:click="toggle()" :aria-expanded="open" :aria-controls="$id('dropdown-button')" type="button" class="flex items-center gap-2 bg-white px-5 py-[9px] rounded-md shadow">
+                            <button type="button" x-ref="button" :aria-expanded="open" :aria-controls="$id('dropdown-button')" type="button" class="flex items-center gap-2 bg-white px-5 py-[10px] rounded-md shadow">
                                 <img class="w-[20px]" src="{{ asset($selectedCountry->flag_url) }}" alt="">
                                 <span>({{ $selectedCountry->dial_code }})</span>
-
 
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -60,7 +64,7 @@
                             </button>
 
                             <!-- Panel -->
-                            <div x-ref="panel" x-show="open" x-transition.origin.top.left x-on:click.outside="close($refs.button)" :id="$id('dropdown-button')" style="display: none;" class="absolute left-0 mt-2 w-40 rounded-md bg-white shadow-md">
+                            <div x-ref="panel" x-show="open" x-transition.origin.top.left x-on:click.outside="close($refs.button)" :id="$id('dropdown-button')" style="display: none;" class="absolute left-0 mt-2 rounded-md bg-white shadow-md">
                                 @foreach($countries as $country)
                                 <a href="#" x-on:click="open = false" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
                                     <img class="w-[20px]" src="{{ asset($country->flag_url) }}" alt="">
@@ -70,6 +74,10 @@
                             </div>
                         </div>
                     </div>
+
+                    <x-text-input id="phone" class="block mt-1 w-[80%]" type="tel" name="phone" value="{{ old('phone')}}" />
+
+
                 </div>
             </div>
             <x-input-error x-show="authwith == 'phone'" :messages="$errors->get('phone')" class="mt-2" />
