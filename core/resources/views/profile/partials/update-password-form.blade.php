a7f7034045dce9e8d5fa6b2f3014926d57c790bb
user.name "bethelhem-kassaw <user.email "bettykassaw@gmail.com>
2024-05-14 01:28:32 +0300

    added the modals and create a dropzone for droping the document

--- core/resources/views/profile/partials/update-password-form.blade.php
+++ core/resources/views/profile/partials/update-password-form.blade.php
@@ -1,42 +1,48 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
+<section class="font-poppins">
+    <button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-edit-password')" class=" flex flex-row gap-3 border-b pb-3 w-full">
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        <div>
+            <img src="{{ asset('assets/icons/vuesax/linear/key.svg') }}" alt="" srcset="">
+
+        </div>
+        <p class="font-medium">
+            Change Password
         </p>
-    </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('put')
 
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-        </div>
+    </button>
+    <x-modal name="confirm-user-edit-password" :show="$errors->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('password.update') }}" class="mt-2 space-y-6 p-6">
+            @csrf
+            @method('put')
+            <p class="font-poppins text-center font-semibold text-lg">
+                Change Password
+            </p>
+            <div>
+                <x-input-label for="update_password_current_password" :value="__('Current Password')" />
+                <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+                <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+            </div>
 
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
+            <div>
+                <x-input-label for="update_password_password" :value="__('New Password')" />
+                <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+            </div>
 
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
+            <div>
+                <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
+                <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+            </div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button class="w-40 py-2 rounded-full flex justify-center">{{ __('Save') }}</x-primary-button>
+            <div class="flex items-center gap-4">
+                <x-primary-button class="w-40 py-2 rounded-full flex justify-center">{{ __('Save') }}</x-primary-button>
 
-            @if (session('status') === 'password-updated')
-            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
+                @if (session('status') === 'password-updated')
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+                @endif
+            </div>
+        </form>
+    </x-modal>
 </section>
