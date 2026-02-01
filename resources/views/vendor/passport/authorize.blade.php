f49cc5e430fdbd03045212f50c4b2143f9c3ad7f
Nesru <nesrusadik6@gmail.com>
2026-02-01 20:24:56 +0300

    react done☘️

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }} - Authorization</title>
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
         
     
     <!-- Font Awesome for icons -->
